52a597a62f689b81f61914e966a345c122bbbabf
jabaaaaa <olli.luukkonen1@gmail.com>
2021-08-16 14:44:58 +0300

    the-phonebook get data from local server implemented

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import AddPerson from './components/addPerson'
 import Numbers from './components/numbers'
 import SearchPerson from './components/searchPerson'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        console.log(response.data); // TODO delete
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
